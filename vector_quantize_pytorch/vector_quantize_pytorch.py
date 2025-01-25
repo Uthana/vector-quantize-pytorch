c6975b4f41b7f19295ca01ab3c108c70974db3e1
Ryan Woodard <r@uthana.com>
2025-01-25 01:15:12 +0000

    Remove calc_commitment_loss_in_eval, sync w/ upstream

--- vector_quantize_pytorch/vector_quantize_pytorch.py
+++ vector_quantize_pytorch/vector_quantize_pytorch.py
@@ -860,7 +860,6 @@ class VectorQuantize(Module):
         affine_param_codebook_decay = 0.9,
         sync_update_v = 0., # the v that controls optimistic vs pessimistic update for synchronous update rule (21) https://minyoungg.github.io/vqtorch/assets/draft_050523.pdf
         return_zeros_for_masked_padding = True,
-        calc_commitment_loss_in_eval = False,
     ):
         super().__init__()
         self.dim = dim
@@ -964,7 +963,6 @@ class VectorQuantize(Module):
 
         # for variable lengthed sequences, whether to take care of masking out the padding to 0 (or return the original input)
         self.return_zeros_for_masked_padding = return_zeros_for_masked_padding
-        self.calc_commitment_loss_in_eval = calc_commitment_loss_in_eval
 
     @property
     def codebook(self):
@@ -1287,19 +1285,6 @@ class VectorQuantize(Module):
                 embed_ind,
                 -1
             )
-
-        if self.calc_commitment_loss_in_eval and not self.training:
-            # eval() mode does not need commitment loss.
-            # For computational efficiency, it does not compute it.
-            # MotionGPT uses it, though, so we add it for comparisons.
-            if x.shape == quantize.shape:
-                # but if we have dim != codebook_dim, that will not work
-                assert commit_loss.item() == 0, commit_loss
-                assert loss.item() == 0, loss
-                loss = commit_loss = F.mse_loss(quantize, x)
-            # else:
-            #     # TODO implement logger.debug() for following
-            #     print("VectorQuantize:  no loss calc, x and quantize shape differ: ", x.shape, quantize.shape)
 
         if not return_loss_breakdown:
             return quantize, embed_ind, loss
